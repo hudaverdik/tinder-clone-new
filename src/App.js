fe34c1ec20eed7d154692158cbc5533e99b319b1
hudaverdik <hudaverdik@itu.edu.tr>
2022-08-13 17:28:32 +0300

    git commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lets build Tinder🏇🏻🏇🏻</h1>
+
+      {/*Header */}
+      <Header/>
+      {/*Tinder Card */}
+      {/*Buttons Below Tinder Cards */}
+      
+      {/*Chats Screen */}
+      {/*Individual Chat Screen */}
+      {/*Header */}
     </div>
   );
 }
